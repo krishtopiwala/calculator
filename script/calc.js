bd2342a67259af7e273728cb61304f14b5edc277
unknown <krishtopiwala1724@gmail.com>
2024-01-13 21:21:53 +0530

    fixed the program

--- script/calc.js
+++ script/calc.js
@@ -1,26 +1,56 @@
 const n1 = document.getElementById("num1");
 const n2 = document.getElementById("num2");
-const result = document.querySelector("#resultPara");
-const btn1 = document.getElementById("btnAdd");
+const res = document.querySelector("#resultPara");
+const addBtn = document.getElementById("btnAdd");
+const subBtn = document.getElementById("btnSub");
+const mulBtn = document.getElementById("btnMul");
+const divide = document.getElementById("btnDivide");
 const resetBtn = document.querySelector("#btnReset");
 
-let sum = 0;
+let getN1 = 0;
+let getN2 = 0;
 
 const reset = () => {
     n1.value = "";
     n2.value = "";
-    sum = 0;
-    result.classList.add("hide");
+    res.classList.add("hide");
 };
 
-const add = () => {
-    const getN1 = Number(n1.value);
-    const getN2 = Number(n2.value);
-    sum = getN1 + getN2;
-    result.classList.remove("hide");
-    result.innerHTML = `<p><strong>Result: </strong> ${sum} </p>`;
-    console.log(sum);
+const result = (answer) => {
+    res.classList.remove("hide");
+    res.innerHTML = `<p><strong>Result: </strong> <u> ${answer} </u></p>`;
 };
 
-btn1.addEventListener("click", add);
+const additonFunction = () => {
+    getN1 = Number(n1.value);
+    getN2 = Number(n2.value);
+    let sum = (getN1 + getN2);
+    result(sum);
+};
+
+const subtractionFunction = () => {
+    getN1 = Number(n1.value);
+    getN2 = Number(n2.value);
+    let sub = (getN1 - getN2);
+    result(sub);
+};
+
+const multiplyFunction = () => {
+    getN1 = Number(n1.value);
+    getN2 = Number(n2.value);
+    let mul = (getN1 * getN2);
+    result(mul);
+}
+
+const divideFunction = () => {
+    getN1 = Number(n1.value);
+    getN2 = Number(n2.value);
+    let divide = (getN1 / getN2);
+    result(divide);
+}
+
+addBtn.addEventListener("click", additonFunction);
+subBtn.addEventListener("click", subtractionFunction);
+mulBtn.addEventListener("click", multiplyFunction);
+divide.addEventListener("click", divideFunction);
 resetBtn.addEventListener("click", reset);
